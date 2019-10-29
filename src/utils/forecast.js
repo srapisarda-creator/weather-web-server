facf4b90abd8bfee308dd49e5940fddf804ae173
srapisarda-creator <56789194+srapisarda-creator@users.noreply.github.com>
2019-10-29 21:46:23 +0000

    changes to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (lat, lon, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.'
+            callback(undefined, body.daily.data[0].summary + 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.  The high today is ' + body.daily.data[0].temperatureHigh + ' , with a low of ' + body.daily.data[0].temperatureLow + '.'
             )
         }
     })
