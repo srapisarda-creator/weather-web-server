03ae48da403ed6ae708de4bfab4a3f40b0364009
srapisarda-creator <56789194+srapisarda-creator@users.noreply.github.com>
2019-10-29 19:41:21 +0000

    Remove unnecessary concole.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side js file - loaded.')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.getElementById('message-1')
